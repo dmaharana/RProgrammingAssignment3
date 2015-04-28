d8ee5c4fa3c42f5122cf026cf743ea414f9b69a9
dmaharana <dmaharana@gmail.com>
2015-04-28 11:49:41 +0530

    add return NA for hospital not found for the searched rank

--- rankhospital.R
+++ rankhospital.R
@@ -1,7 +1,7 @@
 rankhospital <- function(state, outcome, num = 'best'){
   dfCareMeasure <- read.csv('data/outcome-of-care-measures.csv', 
                             colClasses = 'character',
-                            na.strings='NA')
+                            na.strings='Not Available')
   # validate state
   stateList <- unique(dfCareMeasure[,7])
   if (!state %in% stateList) {
@@ -13,17 +13,49 @@ rankhospital <- function(state, outcome, num = 'best'){
     stop('Invalid Outcome')
   }
   
-  hospitalCount <- nrow(unique(dfCareMeasure[,1]))
+  hospitalCount <- nrow(data.frame(unique(dfCareMeasure[,1])))
   if (num == 'best'){
     choiceRank = 1
   }else if (num == 'worst'){
     choiceRank = -1
-  }else if (num > hospitalCount){
-    return (NA)
+  }else if (is.numeric(num) == TRUE){
+    if (num > hospitalCount){
+      return (NA)
+    }else {
+      choiceRank = num
+    }
   }else {
-    choiceRank = num
+    stop('Invalid num')
   }
   
-  print(hospitalCount)
+  # strings to be matched
+  myoutc <- c('mortality', unlist(strsplit(outcome, ' ')))
   
+  OutcomeColName <- names(dfCareMeasure)[grepl('^hospital', names(dfCareMeasure), ignore.case = TRUE)]
+  for (regPat in myoutc){
+    outcomeMatchList <- grepl(regPat, OutcomeColName, ignore.case = TRUE)
+    OutcomeColName <- OutcomeColName[outcomeMatchList]
+  }
+  
+  ## Return hospital name in that state with lowest 30-day death rate
+  dfCareMeasure.state <- dfCareMeasure[dfCareMeasure$State==state,]
+  
+  #get rid of the NA's in the desired outcome column
+  dfCareMeasure.state[, OutcomeColName] <- as.numeric(dfCareMeasure.state[,OutcomeColName])
+  bad <- is.na(dfCareMeasure.state[, OutcomeColName])
+  desired_data <- dfCareMeasure.state[!bad, ]
+  
+  index <- with(desired_data, order(desired_data[OutcomeColName], desired_data['Hospital.Name']))
+  ordered_desired_data <- desired_data[index, ]
+  
+  if (choiceRank == -1){
+    choiceRank = length(ordered_desired_data[, OutcomeColName])
+  }
+  
+  if(choiceRank <= nrow(ordered_desired_data)){
+    ordered_desired_data[choiceRank, 'Hospital.Name']  
+  }else{
+    return (NA)
+  }
+
 }
